c7c0b5a61a90571486f4a5b8b68af2bcde7a384a
rafaeldreyer <dreyerafael@gmail.com>
2023-08-18 02:11:28 -0300

    include templates/contato

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- Estilos do projeto -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        Template cabeçalho
+    </header>
