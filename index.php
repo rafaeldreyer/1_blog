d3653d496c868a18a9c7ca3ada99059349cbacc1
rafaeldreyer <dreyerafael@gmail.com>
2023-08-18 01:51:44 -0300

    include helpers/css

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php
+    include_once("helpers/url.php");
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Blog codar</title>
+    <link rel="stylesheet" href="<?php $BASE_URL ?>/css/style.css">
 </head>
 <body>
     
