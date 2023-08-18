dcab9cb784f809f324447a9cf1f4bedba32f52b8
rafaeldreyer <dreyerafael@gmail.com>
2023-08-18 10:12:31 -0300

    backend/posts

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,7 @@
 <?php
     include_once("helpers/url.php");
+    include_once("data/posts.php");
+    include_once("data/categories.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt_br">
@@ -7,9 +9,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <!-- Estilos do projeto -->
+    <!-- ESTILOS DO PROJETO -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
-    <!-- Google fonts -->
+    <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
